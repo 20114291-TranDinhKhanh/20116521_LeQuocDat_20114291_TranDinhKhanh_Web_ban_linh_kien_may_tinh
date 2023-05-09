fe7c32217ec3b566f7d284f2a73f1ae88082befd
le-dat <lqdat87@gmail.com>
2023-05-09 16:51:36 +0700

    fix url 2

--- js/main2.js
+++ js/main2.js
@@ -11,7 +11,7 @@ let getUser = async () => {
             <div class="container py-2">
                 <div class="row">
                     <div class="col-8">
-                      <a href="/"><img src="/img/icon.png" style="width:100px; height:65px;" /></a>
+                      <a href="/"><img src="../img/icon.png" style="width:100px; height:65px;" /></a>
                     </div>
                     <div class="col-4 ">
                         <nav class="navbar-expand d-flex justify-content-end">
