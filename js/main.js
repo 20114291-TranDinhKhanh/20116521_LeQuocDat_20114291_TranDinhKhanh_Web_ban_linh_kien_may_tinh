f29a6f71460b483c3f4eb747122ac4f602e1ae36
le-dat <lqdat87@gmail.com>
2023-05-09 16:52:42 +0700

    fix url 3

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let getUser = async () => {
             <div class="container py-2">
                 <div class="row">
                     <div class="col-8 flex">
-                      <a href="/"><img src="../img/icon.png" style="width:100px; height:65px;" /></a>
+                      <a href="/"><img src="./img/icon.png" style="width:100px; height:65px;" /></a>
                       <span>STUDIO</span>
                     </div>
                     <div class="col-4 ">
@@ -25,18 +25,18 @@ let getUser = async () => {
                                   </li>
                                     `
                                     : `<li class="text-nowrap">
-                                    <a class="nav-link nav__header" href="../html/dangnhap.html">
+                                    <a class="nav-link nav__header" href="./html/dangnhap.html">
                                       Đăng nhập
                                     </a>
                                   </li>
                                   <li class="nav-item text-nowrap">
-                                    <a class="nav-link nav__header" href="../html/dangki.html">
+                                    <a class="nav-link nav__header" href="./html/dangki.html">
                                       Đăng kí
                                     </a>
                                   </li>`
                                 }
                                 <li class="nav-item text-nowrap">
-                                    <a class="nav-link  icon__cart" href="../html/store.html">
+                                    <a class="nav-link  icon__cart" href="./html/store.html">
                                         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart4" fill="grey" xmlns="http://www.w3.org/2000/svg">
                                           <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                                       </svg>
