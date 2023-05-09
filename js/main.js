3b9140108a15d06643e388147e278734d144f73f
le-dat <lqdat87@gmail.com>
2023-05-09 16:54:15 +0700

    fix url 4

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ let getUser = async () => {
                                   </li>`
                                 }
                                 <li class="nav-item text-nowrap">
-                                    <a class="nav-link  icon__cart" href="./html/store.html">
+                                <a class="nav-link  icon__cart" href="https://20114291-trandinhkhanh.github.io/20116529_LeQuocDat_20114291_TranDinhKhanh_Web_ban_linh_kien_may_tinh/html/store.html">
                                         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart4" fill="grey" xmlns="http://www.w3.org/2000/svg">
                                           <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                                       </svg>
